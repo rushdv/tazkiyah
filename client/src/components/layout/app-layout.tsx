7fb0ad44a504e435038baefb5e1b4657a1d58a3d
rushdv <shihab.zn4@gmail.com>
2026-07-18 23:50:13 +0600

    feat(layout): enhance AppLayout with background styling and improved main content structure

--- client/src/components/layout/app-layout.tsx
+++ client/src/components/layout/app-layout.tsx
@@ -3,11 +3,14 @@ import { Sidebar } from './sidebar';
 
 export function AppLayout() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen bg-background">
       <Sidebar />
-      <main className="ml-64 flex-1 p-8">
-        <Outlet />
+      <main className="ml-64 flex-1 overflow-x-hidden p-8 md:p-10 lg:p-12">
+        <div className="mx-auto max-w-7xl">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
 }
+
