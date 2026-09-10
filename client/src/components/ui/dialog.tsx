2c9467704ab1c9cc0c6e5b0bcf2484fb6428e603
rushdv <shihab.zn4@gmail.com>
2026-09-10 22:39:50 +0600

    feat(ui): add DialogFooter export to Radix dialog UI component

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -76,4 +76,9 @@ const DialogDescription = forwardRef<
 ));
 DialogDescription.displayName = 'DialogDescription';
 
-export { Dialog, DialogTrigger, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogDescription };
+const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
+);
+DialogFooter.displayName = 'DialogFooter';
+
+export { Dialog, DialogTrigger, DialogClose, DialogContent, DialogHeader, DialogFooter, DialogTitle, DialogDescription };
